eb8dcedbc33096e266c6f4ef472dad434df10bb0
Ines Ouled Elhaj <ines.ouledelhaj@insat.u-carthage.tn>
2021-11-12 13:22:22 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from App Engine!');
+});
+
+app.get('/factorial/:number', (req, res) => {
+    var result = 1;
+    for(i=1;i<=req.params.number;i++){
+        result = result*i;
+    }
+    res.send(`${result}`);
+  });
+
+// Listen to the App Engine-specified port, or 8080 otherwise
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
